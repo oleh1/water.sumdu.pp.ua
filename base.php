fb485661d2bb855108aba98f4f9fcc06183ed1bb
oleh1 <lyboleg@gmail.com>
2017-04-20 20:22:19 +0300

    fix

--- base.php
+++ base.php
@@ -14,31 +14,36 @@ if($_POST['url'] == 'registration') {
   $pass_r = $_POST['pass_r'];
   if ($pass && $pass_r) {
     if ($pass == $pass_r) {
-      $password = $pass;
-      $e = true;
+      $pas = $pass;
     } else {
       header('Location: http://' . $_SERVER["HTTP_HOST"] . '/' . $url . '?n=10');
     }
   }
 
-  $r = $pdo->query("SELECT * FROM users WHERE user_name = $name OR mail = $mail");
+  $r = $pdo->prepare("SELECT * FROM users WHERE user_name = :user_name OR mail = :mail");
+  $r->bindParam(':user_name', $user_name);
+  $r->bindParam(':mail', $mail);
+  $r->execute();
   $r = $r->fetchAll();
+  var_dump($r);
+
   if ($r) {
     header('Location: http://' . $_SERVER["HTTP_HOST"] . '/' . $url . '?n=2');
   } else {
-    $pdo->query("INSERT INTO users (user_name, mail, pass)VALUES($name, $mail, $password)");
-    if ($e) {
-      header('Location: http://' . $_SERVER["HTTP_HOST"] . '/' . $url . '?n=1');
-    }
+    $pdo->query("INSERT INTO users VALUES(null, '{$name}', '{$mail}', '{$pas}')");
+    header('Location: http://' . $_SERVER["HTTP_HOST"] . '/' . $url . '?n=1');
   }
 }
 
 if($_POST['url'] == 'login') {
   $url = $_POST['url'];
   $name = $_POST['name'];
-  $pass = $_POST['pass'];
+  $pas = $_POST['pass'];
 
-  $r = $pdo->query("SELECT * FROM users WHERE user_name = $name OR pass = $pass");
+  $r = $pdo->prepare("SELECT * FROM users WHERE user_name = :user_name OR pass = :pass");
+  $r->bindParam(':user_name', $user_name);
+  $r->bindParam(':pass', $pas);
+  $r->execute();
   $r = $r->fetchAll();
   if ($r) {
     session_start();
