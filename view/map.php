5cc827e9e1a65c40a0c509616d2681f3dcf6cc9d
oleh1 <lyboleg@gmail.com>
2017-06-07 11:13:19 +0300

    wd

--- view/map.php
+++ view/map.php
@@ -6,16 +6,16 @@
     <div><input class="gps" type="text" name="gps" placeholder="Введите адрес"><input type="submit" value="Получить результат"></div>
     <table class="ta">
     <tr>
-      <th>Водопровод <input name="a1" value="a1" type="checkbox"></th>
-      <th>Колодец <input name="a2" value="a2" type="checkbox"></th>
-      <th>Скважена <input name="a3" value="a3" type="checkbox"></th>
-      <th>Другие источники <input name="a4" value="a4" type="checkbox"></th>
+      <th>Водопровод <input name="a" value="1" type="checkbox"></th>
+      <th>Колодец <input name="a" value="2" type="checkbox"></th>
+      <th>Скважина <input name="a" value="3" type="checkbox"></th>
+      <th>Другие источники <input name="a" value="4" type="checkbox"></th>
     </tr>
     <tr>
-      <th>Питьевая <input name="b1" value="b1" type="checkbox"></th>
-      <th>Битовая <input name="b2" value="b2" type="checkbox"></th>
-      <th>Промишленная <input name="b3" value="b3" type="checkbox"></th>
-      <th>Другое <input name="b4" value="b4" type="checkbox"></th>
+      <th>Питьевая <input name="b" value="1" type="checkbox"></th>
+      <th>Бытовая <input name="b" value="2" type="checkbox"></th>
+      <th>Промышленная <input name="b" value="3" type="checkbox"></th>
+      <th>Другое <input name="b" value="4" type="checkbox"></th>
     </tr>
     </table>
     <div class="map">
@@ -26,13 +26,7 @@
 
   <?php
 
-  if ( $_POST['map'] == 'map' ){
-
-    $c = include 'config.php';
-    $h = $c['host'];
-    $db = $c['dbname'];
-    $u = $c['user'];
-    $p = $c['pass'];
+  /*if ( $_POST['map'] == 'map' ){
 
     $gps =  $_POST['gps'];
     $a1 =  $_POST['a1'];
@@ -72,7 +66,7 @@
       $k3 = '33%';
 
       $ooo = 'Хорошая';
-    }
+    }*/
 
 //    var_dump($r);
 //    $pdo = new PDO("mysql:host=$h;dbname=$db", $u, $p);
@@ -92,9 +86,43 @@
 
 //    $r = mysqli_query($mysqli, "SELECT * FROM `city` `c` JOIN `kind` `k`ON `c`.`kind_id` = `k`.`id_k` JOIN `result` `r` ON `k`.`result_id` = `r`.`id_r` WHERE `name_city` LIKE '%{$gps}%'");
 
+//  }
 
+  $c = include 'config.php';
+  $h = $c['host'];
+  $db = $c['dbname'];
+  $u = $c['user'];
+  $p = $c['pass'];
+  $pdo = new PDO("mysql:host=$h;dbname=$db", $u, $p);
+
+  if ( $_POST['map'] == 'map' && $_POST['a'] && $_POST['b'] == 1 && $_POST['gps'] ) {
+
+    $gps = $_POST['gps'];
+    $a = $_POST['a'];
+
+    $b = $_POST['b'];
+
+    $db = $pdo->query("SELECT * FROM `result` WHERE `city` LIKE '%{$gps}%' AND `king` = '{$a}'");
+    $result = $db->fetchAll();
+//  var_dump($result);
 
   }
+
+//  $pdo->query("INSERT INTO result VALUES(null, '83%', '0%', '17%', '83%', '0%', '17%', '67%', '0%', '33%', '50%', '33%', '17%', '33%', '17%', '50%', '67%', '0%', '33%', '0%', '67%', '3%', 'Чистая', 1, 'г. Сумы')");
+
+//  $pdo->query("INSERT INTO result VALUES(null, '13%', '50%', '38%', '100%', '0%', '0%', '10%', '0%', '90%', '90%', '0%', '10%', '13%', '0%', '88%', '63%', '25%', '13%', '57%', '0%', '43%', 'Чистая', 2, 'г. Сумы')");
+
+//  $pdo->query("INSERT INTO result VALUES(null, '100%', '0%', '0%', '100%', '0%', '0%', '100%', '0%', '0%', '0%', '0%', '100%', '0%', '100%', '0%', '0%', '6%', '100%', '', '', '', 'Чистая', 3, 'г. Конотоп, Сумская обл.')");
+
+//  $pdo->query("INSERT INTO result VALUES(null, '100%', '0%', '0%', '100%', '0%', '0%', '100%', '0%', '0%', '0%', '100%', '0%', '100%', '0%', '0%', '0%', '100%', '0%', '100%', '0%', '0%', 'Чистая', 3, 'г. Глухов, Сумская обл., ул. Киевомосковская, 4')");
+
+//  $pdo->query("INSERT INTO result VALUES(null, '100%', '0%', '0%', '100%', '0%', '0%', '100%', '0%', '0%', '0%', '100%', '0%', '100%', '0%', '0%', '0%', '100%', '0%', '100%', '0%', '0%', 'Чистая', 3, 'с. Алтыновка, Сумская обл.')");
+
+//  $pdo->query("INSERT INTO result VALUES(null, '100%', '0%', '0%', '100%', '0%', '0%', '100%', '0%', '0%', '100%', '0%', '0%', '100%', '0%', '0%', '100%', '0%', '0%', '100%', '0%', '0%', 'Чистая', 3, 'г. Сумы, ул. Ивана Франко 34')");
+
+//  $pdo->query("INSERT INTO result VALUES(null, '55%', '45%', '0%', '34%', '60%', '6%', '78%', '13%', '9%', '45%', '55%', '0%', '35%', '60%', '5%', '46%', '50%', '4%', '40%', '60%', '0%', 'Умеренная', 3, 'с. Боромле, Сумская обл.')");
+
+
 
   ?>
 
@@ -110,51 +138,51 @@
         </tr>
         <tr>
           <th>Сухой остаток</th>
-          <th><?php echo $z1; ?></th>
-          <th><?php echo $z2; ?></th>
-          <th><?php echo $z3; ?></th>
+          <th><?php echo $result[0]["dry_ostatok_?lean"]; ?></th>
+          <th><?php echo $result[0]["dry_ostatok_?ontaminants"]; ?></th>
+          <th><?php echo $result[0]["dry_ostatok_dirty"]; ?></th>
         </tr>
         <tr>
           <th>Нитраты</th>
-          <th><?php echo $x1; ?></th>
-          <th><?php echo $x2; ?></th>
-          <th><?php echo $x3; ?></th>
+          <th><?php echo $result[0]["nitrates_?lean"]; ?></th>
+          <th><?php echo $result[0]["nitrates_?ontaminants"]; ?></th>
+          <th><?php echo $result[0]["nitrates_dirty"]; ?></th>
         </tr>
         <tr>
           <th>Железо общее</th>
-          <th><?php echo $c1; ?></th>
-          <th><?php echo $c2; ?></th>
-          <th><?php echo $c3; ?></th>
+          <th><?php echo $result[0]["iron_total_?lean"]; ?></th>
+          <th><?php echo $result[0]["iron_total_?ontaminants"]; ?></th>
+          <th><?php echo $result[0]["iron_total_dirty"]; ?></th>
         </tr>
         <tr>
           <th>Жесткость</th>
-          <th><?php echo $v1; ?></th>
-          <th><?php echo $v2; ?></th>
-          <th><?php echo $v3; ?></th>
+          <th><?php echo $result[0]["rigidity_?lean"]; ?></th>
+          <th><?php echo $result[0]["rigidity_?ontaminants"]; ?></th>
+          <th><?php echo $result[0]["rigidity_dirty"]; ?></th>
         </tr>
         <tr>
           <th>Мутность</th>
-          <th><?php echo $n1; ?></th>
-          <th><?php echo $n2; ?></th>
-          <th><?php echo $n3; ?></th>
+          <th><?php echo $result[0]["turbidity_?lean"]; ?></th>
+          <th><?php echo $result[0]["turbidity_?ontaminants"]; ?></th>
+          <th><?php echo $result[0]["turbidity_dirty"]; ?></th>
         </tr>
         <tr>
           <th>Цветность</th>
-          <th><?php echo $m1; ?></th>
-          <th><?php echo $m2; ?></th>
-          <th><?php echo $m3; ?></th>
+          <th><?php echo $result[0]["color_?lean"]; ?></th>
+          <th><?php echo $result[0]["color_?ontaminants"]; ?></th>
+          <th><?php echo $result[0]["color_dirty"]; ?></th>
         </tr>
         <tr>
           <th>Запах</th>
-          <th><?php echo $k1; ?></th>
-          <th><?php echo $k2; ?></th>
-          <th><?php echo $k3; ?></th>
+          <th><?php echo $result[0]["smell_?lean"]; ?></th>
+          <th><?php echo $result[0]["smell_?ontaminants"]; ?></th>
+          <th><?php echo $result[0]["smell_dirty"]; ?></th>
         </tr>
       </table>
     </div>
     <div class="obw">
       <div>Общее состояние:</div>
-      <div><strong><?php echo $ooo; ?></strong></div>
+      <div><strong><?php echo $result[0]["conclusion"]; ?></strong></div>
     </div>
   </div>
 
